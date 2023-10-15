3dc23c1c58a89512715dd8bcb3797843472a8212
DianeLantrann <diane.lantran@gmail.com>
2023-10-15 18:56:27 -0400

    preprocess data et PCA

--- main.py
+++ main.py
@@ -9,34 +9,38 @@ import preprocessing as prep
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif  # Vous pouvez utiliser une autre fonction de score
 
-FILE_PATH_R = "/data/winequality-red.csv"
-FILE_PATH_W = "/data/winequality-white.csv"
-DATASET_R = pd.read_csv(FILE_PATH_R)
-DATASET_W = pd.read_csv(FILE_PATH_W)
+FILE_PATH_R = "winequality-red.csv"
+FILE_PATH_W = "winequality-white.csv"
+DATASET_R = pd.read_csv(FILE_PATH_R, sep=';')
+DATASET_W = pd.read_csv(FILE_PATH_W, sep=';')
 
 
-# nettoyage des données (<70% de données sur lignes et colones)
-df = dataTreatmentUtils.removeUselessColumns(DATASET, 30)
-df = df.drop("Summary", axis=1)
-df = df.drop("Registration", axis=1)
-df = dataTreatmentUtils.removeUselessRows(df, 25)
+## Nettoyage des données (<70% de données sur lignes et colones)
+df_r = dataTreatmentUtils.removeUselessColumns(DATASET_R, 30)
+df_w = dataTreatmentUtils.removeUselessColumns(DATASET_W, 30)
+#aucune donnée manquante
 
-# Preprocessing
-df = prep.simplifyDate(df)
-df = prep.simplifyLocation(df)
-df = prep.simplifyRoute(df)
-df = prep.colToOrdinal(df, ["Location", "Operator", 
-                            "AC Type", "Departure", 
-                            "Arrival", "cn/ln"])
 
-# Standardization
-df = prep.standardize(df)
+## Preprocessing
+# on a que des colonnes avec des nombres donc pas besoin
 
-# PCA
-reducData_PCA = mathsUtils.PCA(df, 0.05) #les valeurs propres < 5% ne sont pas prises en compte
-print(reducData_PCA)
+## Standardization
+df_r = prep.standardize(df_r)
+df_w = prep.standardize(df_w)
 
-#Comparaison avec SelectKBest
-k_best = SelectKBest(score_func=f_classif, k=len(df.columns))  # k = nombre de caractéristiques souhaité
-reducData_SB = k_best.fit_transform(df, None) #étiquettes de classe cibles = None -> non supervisé
-print(reducData_SB)
+## PCA
+threshold = 0.1 #les valeurs propres < 10% ne sont pas prises en compte
+data_PCA_r = mathsUtils.PCA(df_r, threshold) 
+data_PCA_w = mathsUtils.PCA(df_w, threshold) 
+
+print("red : ")
+print(data_PCA_r)
+
+## Comparaison avec SelectKBest
+# k_best_r = SelectKBest(score_func=f_classif, k=len(df_r.columns))  # k = nombre de caractéristiques souhaitées
+# reducData_SB_r = k_best_r.fit_transform(df_r, None) #étiquettes de classe cibles = None -> non supervisé
+# print(reducData_SB_r)
+
+# k_best_w = SelectKBest(score_func=f_classif, k=len(df_w.columns))  # k = nombre de caractéristiques souhaitées
+# reducData_SB_w = k_best_w.fit_transform(df_w, None) #étiquettes de classe cibles = None -> non supervisé
+# print(reducData_SB_w)
--- preprocessing.py
+++ preprocessing.py
@@ -9,39 +9,6 @@ import re
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
 
-def splitDate(date):
-    month, day, year = map(int, date.split('/'))
-    return month, year
-
-def simplifyDate(df):
-    df[['Month', 'Year']] = df['Date'].apply(lambda x: pd.Series(splitDate(x)))
-    df = df.drop('Date', axis=1)
-    return df
-
-def getBroadestLocation(location):
-    return str(location).split(",")[-1].strip()
-
-def simplifyLocation(df):
-    # Get broadest location
-    df['Location'] = df['Location'].apply(getBroadestLocation)  
-    return df
-
-def splitRoute(route):
-    if (pd.notna(route)):
-        routeArray = re.split(r'(\s?-\s)|(\s-\s?)', route.strip())
-        routeArray = [routeArray[0], routeArray[-1]]
-        departure, arrival = list(map(lambda x: x.split(",")[0], routeArray))
-        #print(re.split(r'\s-\s', route.strip()))
-    else:
-        departure = arrival = float("nan")
-    return departure, arrival
-        
-
-def simplifyRoute(df):
-    df[['Departure', 'Arrival']] = df['Route'].apply(lambda x: pd.Series(splitRoute(x)))
-    df = df.drop('Route', axis=1)
-    return df
-
 def colToOrdinal(df, colnames):
     # Prepare encoder object
     encoder = OrdinalEncoder(encoded_missing_value=-1)
